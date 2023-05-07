94d1d39fe0b19dfd6138728baf642aa60b6058a2
Elizabeth Jimenez <2good2btru@Elizabeths-MacBook-Air-3.local>
2023-05-06 23:53:58 -0400

    Added variables to declare

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,11 @@
 const searchBar = document.getElementById('search-bar');
 const searchButton = document.getElementById('search-button');
-const clearHistory = document.getElementById('clear-history');
+const clearButton = document.getElementById('clear-history');
+const currentCity = document.getElementById('current-city');
+const currentTemperature = document.getElementById('temperature');
+const currentHumidty= document.getElementById('humidity');
+const currentWSpeed= document.getElementById('wind-speed');
+let city =""
 
 document.getElementById('search-button').addEventListener('click', function(){
     console.log(searchBar.value);
@@ -8,3 +13,4 @@ document.getElementById('search-button').addEventListener('click', function(){
 document.getElementById('clear-history').addEventListener('click', function(){
     console.log(searchBar.value);
 })
+let sCity=[];
