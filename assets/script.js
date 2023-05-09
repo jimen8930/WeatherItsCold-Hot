49a51c1fcfe7e9cf491745b06c34a127ac82d1eb
Elizabeth Jimenez <2good2btru@Elizabeths-MacBook-Air-3.local>
2023-05-09 17:04:53 -0400

    Added a fetch to call api key

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+
 const searchBar = document.getElementById('search-bar');
 const searchButton = document.getElementById('search-button');
 const clearButton = document.getElementById('clear-history');
@@ -5,12 +6,33 @@ const currentCity = document.getElementById('current-city');
 const currentTemperature = document.getElementById('temperature');
 const currentHumidty= document.getElementById('humidity');
 const currentWSpeed= document.getElementById('wind-speed');
-let city =""
+let APIKey = "95719a559a5afa0b90a3b42001df26ad"
 
-document.getElementById('search-button').addEventListener('click', function(){
-    console.log(searchBar.value);
+// document.getElementById('search-button').addEventListener('click', function(){
+//     console.log(searchBar.value);
+// })
+// document.getElementById('clear-history').addEventListener('click', function(){
+//     console.log(searchBar.value);
+// })
+
+
+ function onClick() {
+   
+    fetch("http://api.openweathermap.org/data/2.5/weather?q=" + searchBar.value + "&appid=" + APIKey).then(function(res){
+    return res.json()
+}).then(function(data){
+    console.log(data);
+    console.log(data.coord.lat);
+}).catch(function(err){
+    console.log(err);
 })
-document.getElementById('clear-history').addEventListener('click', function(){
-    console.log(searchBar.value);
-})
-let sCity=[];
+    
+    
+ }
+
+
+ 
+
+
+    
+
