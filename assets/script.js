43d7feb3b497ae7812421df5fad447f7615bc125
Elizabeth Jimenez <2good2btru@Elizabeths-MacBook-Air-3.local>
2023-05-11 18:24:27 -0400

    Remove the for loop and added another for loop which allow it to loop through all five days.

--- assets/script.js
+++ assets/script.js
@@ -4,9 +4,9 @@ const searchButton = document.getElementById('search-button');
 const clearButton = document.getElementById('clear-history');
 const currentCity = document.getElementById('current-city');
 const currentTemperature = document.getElementById('temperature');
-const currentHumidty= document.getElementById('humidity');
-const currentWSpeed= document.getElementById('wind-speed');
-const searchHistory=document.getElementById('search-history');
+const currentHumidty = document.getElementById('humidity');
+const currentWSpeed = document.getElementById('wind-speed');
+const searchHistory = document.getElementById('search-history');
 let APIKey = "95719a559a5afa0b90a3b42001df26ad"
 
  document.getElementById('search-button').addEventListener('click', function(){
@@ -37,9 +37,9 @@ function onClick() {
       fetchWeather(city);
       searchBar.value = '';
     }
-    var values = $(this).siblings('.description').val()
-    var time = $(this).parent().attr('id')
-    localStorage.setItem(city, values)
+    // var values = $(this).siblings('.description').val()
+    // var time = $(this).parent().attr('id')
+    // localStorage.setItem(city, values)
   }
   
   // function onClear() {
@@ -53,6 +53,7 @@ function onClick() {
       })
       .then(function(data) {
         displayWeather(data);
+        console.log(data);
       })
       .catch(function(error) {
         console.log('Error fetching weather data:', error);
@@ -81,21 +82,32 @@ function onClick() {
   
     const forecastElements = document.getElementsByClassName('forecast');
   
-    for (let i = 1; i < forecastElements.length + 1; i++) {
+    for (let i = 0; i < forecastElements.length; i++) {
       const forecast = data.list[i * 8];
-      const forecastDateElement = document.getElementById(`fDate${i - 1}`);
-      const forecastImgElement = document.getElementById(`fImg${i - 1}`);
-      const forecastTempElement = document.getElementById(`fTemp${i - 1}`);
-      const forecastHumidityElement = document.getElementById(`fHumidity${i - 1}`);
-      const forecastWindElement = document.getElementById(`fWind${i - 1}`);
-  
+      const forecastDateElement = document.getElementById(`fDate${i}`);
+      const forecastImgElement = document.getElementById(`fImg${i}`);
+      const forecastTempElement = document.getElementById(`fTemp${i}`);
+      const forecastHumidityElement = document.getElementById(`fHumidity${i}`);
+      const forecastWindElement = document.getElementById(`fWind${i}`);
+    
       forecastDateElement.textContent = forecast.dt_txt.substring(0, 10);
       forecastImgElement.innerHTML = `<img src="https://openweathermap.org/img/w/${forecast.weather[0].icon}.png" alt="${forecast.weather[0].description}">`;
       forecastTempElement.textContent = forecast.main.temp + '°F';
       forecastHumidityElement.textContent = forecast.main.humidity + '%';
       forecastWindElement.textContent = forecast.wind.speed + ' m/s';
     }
+    
   }
+  //clear history button
+  let clearHistory = document.getElementById('clear-history');
+  clearHistory.onClick =function() {
+    localStorage.clear();
+    clearSeeachHistoryUI();
+  }
+   function clearSeeachHistoryUI() {
+    let cityElements =[cityOne, cityTwo, cityThree, cityFour, cityFive, citySix, citySeven, cityEight];
+    cityElements.forEach
+   }
   
  
   
